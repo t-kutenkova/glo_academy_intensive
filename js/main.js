4c3ec9bcb54a62979e41afe6450854131597c72e
t-kutenkova <t.v.kutenkova.mac@gmail.com>
2021-02-15 11:07:18 +0300

    L-02: Slider 2 added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-const flowersSlider = new Swiper('.swiper-container', {
+const flowersSlider = new Swiper('.flowers-slider', {
   // Параметры контейнера
   loop: true,
   slidesPerView: 5,
+
+  //Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const feedbackSlider = new Swiper('.feedback-slider', {
+  // Параметры контейнера
+  loop: true,
 
   //Стрелки
   navigation: {
