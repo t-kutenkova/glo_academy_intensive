68c9b3ea727249cf6d5568d4434667f354d59c66
t-kutenkova <t.v.kutenkova.mac@gmail.com>
2021-02-18 10:38:20 +0300

    L-04: Footer + adaptability added

--- js/main.js
+++ js/main.js
@@ -3,11 +3,31 @@ const flowersSlider = new Swiper('.flowers-slider', {
   loop: true,
   slidesPerView: 5,
 
-  //Стрелки
+  // Стрелки
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  // Настройка адаптивности
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  }
+
 });
 
 const feedbackSlider = new Swiper('.feedback-slider', {
